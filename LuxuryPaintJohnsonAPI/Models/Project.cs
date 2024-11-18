5666c07b6495cd9c019faed79f8d2f10a91ae655
Jo Johnson <55406785+jojohnson-jsj@users.noreply.github.com>
2024-11-17 16:50:42 -0800

    Delete LuxuryPaintJohnsonAPI directory

--- LuxuryPaintJohnsonAPI/Models/Project.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LuxuryPaintJohnsonAPI.Models;
-
-public class Project
-{
-	public int Id { get; set; }
-	public string? Title { get; set; }
-	public DateTime CreatedAt { get; set; }
-	public IEnumerable<Photo>? Photos { get; set; }
-}
